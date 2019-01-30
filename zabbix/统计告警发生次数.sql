0bbb793cacc647437e9ebcf778f92e61fb397160
liuyjishg <33978477+liuyjishg@users.noreply.github.com>
2019-01-30 10:33:01 +0800

    Update 统计告警发生次数.sql

--- zabbix/统计告警发生次数.sql
+++ zabbix/统计告警发生次数.sql
@@ -1,27 +1,36 @@
 SELECT
-	g.`name` AS "host_group",
-	h.`name` AS "host",
-	p.`name` AS "triggers",
-	p.severity AS "告警级别",
-	count(*)
+	a.host_group,
+	a. HOST,
+	a.`triggers`,
+	a.count
 FROM
-	problem p,
-	functions f,
-	items i,
-	`hosts` h,
-	hosts_groups hg,
-	hstgrp g
-WHERE
-	p.objectid = f.triggerid
-AND f.itemid = i.itemid
-AND i.hostid = h.hostid
-AND hg.hostid = h.hostid
-AND hg.groupid = g.groupid
-AND p.clock > (
-	UNIX_TIMESTAMP(NOW()) - 2678400
-)
-GROUP BY
-	g.`name`,
-	h.`name`,
-	p.severity,
-	p.`name`
+	(
+		SELECT
+			g.`name` AS "host_group",
+			h.`name` AS "host",
+			p.`name` AS "triggers",
+			count(*) AS count
+		FROM
+			`events` p,
+			functions f,
+			items i,
+			`hosts` h,
+			hosts_groups hg,
+			hstgrp g
+		WHERE
+			p.objectid = f.triggerid
+		AND f.itemid = i.itemid
+		AND i.hostid = h.hostid
+		AND hg.hostid = h.hostid
+		AND hg.groupid = g.groupid
+		AND p.severity <> 0
+		AND p.clock > (
+			UNIX_TIMESTAMP(NOW()) - 2678400
+		)
+		GROUP BY
+			g.`name`,
+			h.`name`,
+			p.`name`
+	) a
+ORDER BY
+	a.count DESC
